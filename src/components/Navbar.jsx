fb4f37e0a5487aec6e3972aad085d425fe5d3909
sumonaung23 <sumonaung575@gmail.com>
2023-11-21 23:54:02 +0630

    created and added packages for crypto app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, MenuOutlined } from '@ant-design/icons'
+
+const Navbar = () => {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export default Navbar
