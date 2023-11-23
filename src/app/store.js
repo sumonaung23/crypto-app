943431acefadea06834d8d7cd2060e3afffc76ea
sumonaung23 <sumonaung575@gmail.com>
2023-11-23 23:57:09 +0630

    created app folder for store.js and added cryptoApi and store the data

---
+++ src/app/store.js
@@ -0,0 +1,21 @@
+/* import { configureStore } from "@reduxjs/toolkit";
+
+import { cryptoApi } from "../services/cryptoApi";
+
+export default configureStore({
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+}) */
+
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../services/cryptoApi";
+
+export default configureStore({
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(cryptoApi.middleware),
+});
