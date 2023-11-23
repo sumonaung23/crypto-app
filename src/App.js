25520680e5bf903a45dce039a546ccd15f7114bc
sumonaung23 <sumonaung575@gmail.com>
2023-11-23 23:54:11 +0630

    imported components and structrued the page with them

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from "antd";
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components/index';
+
+import './App.css';
 
 const App = () => {
     return (
         <div className='app'>
             <div className='navbar'>
-
+                <Navbar />
             </div>
 
             <div className='main'>
-                
-            </div>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path='/' element={<Homepage />} />
+                            <Route exact path='/exchanges' element={<Exchanges />} />
+                            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route exact path='/news' element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
 
-            <div className='footer'>
-                
+                <div className='footer'>
+                    <Typography.Title level={5} style={{ color: '#fff', textAlign: 'ceter'}}>
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
