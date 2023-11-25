85d0aa84d450a1b449b750e793a439fb36b97619
sumonaung23 <sumonaung575@gmail.com>
2023-11-25 22:01:10 +0630

    created api for cryptocoins

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -13,11 +13,8 @@ export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl}),
     endpoints: (builder) => ({
-        /* getCryptos: builder.query({
-            query: () => createRequest('/coins')
-        }) */
         getCryptos: builder.query({
-            query: () => createRequest('/coins'),
+            query: (count) => createRequest(`/coins?limit=${count}`),
         }),
     })
 })
