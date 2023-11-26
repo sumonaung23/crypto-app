33791033e18fba1ec13c48a6588ade935fa70f18
sumonaung23 <sumonaung575@gmail.com>
2023-11-26 23:51:20 +0630

    modified Headers as the previous was removed long ago from source

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,30 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const cryptoNewsApiHeaders = {
+/* const cryptoNewsApiHeaders = {
     'X-BingApis-SDK': 'true',
     'X-RapidAPI-Key': 'c8a42e4734mshd8a3fa3018a4094p1ff31cjsndf003e485402',
     'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
 }
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'; */
+
+const cryptoNewsApiHeaders ={
+    'X-RapidAPI-Key': 'c8a42e4734mshd8a3fa3018a4094p1ff31cjsndf003e485402',
+    'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
+}
+
+const baseUrl = 'https://crypto-news16.p.rapidapi.com'
 
 const createRequest = (url) => ({url, headers: cryptoNewsApiHeaders})
-
-/* const createRequest = ({ newsCategory, count }) => {
-    const encodedCategory = encodeURIComponent(newsCategory);
-    const fullUrl = `/news/search?q=${encodedCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`;
-    console.log('Request URL:', baseUrl + fullUrl);
-    return { url: fullUrl, headers: cryptoNewsApiHeaders };
-}; */
-
-
 
 export const cryptoNewsApi = createApi ({
     reducerPath: 'cryptoNewsApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
-            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+            /* query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`), */
+            query: () => createRequest('/news/top/10')
         })
     })
 })
